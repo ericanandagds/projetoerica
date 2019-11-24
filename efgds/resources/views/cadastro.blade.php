8ffc8ae179a4408b03ed3b4b5f83952b46158ded
ericanandagds <ericanandagds@gmail.com>
2019-11-23 23:46:28 -0300

    pizza tela

--- efgds/resources/views/cadastro.blade.php
+++ efgds/resources/views/cadastro.blade.php
@@ -22,8 +22,9 @@
     <a class="navbar-brand" href="contato.html">Contato</a>
 </nav>
 <section>  
-<form action="home.html" method="post" class="cadastro">
-<h2>Cadastro</h2>
+<form action=url method="post" class="cadastro">
+   @csrf
+  <h2>Cadastro</h2>
   <div class="form-row" name="cadastro">
     <div class="form-group col-md-6">
             <label for="inputEmail4">Email</label>
@@ -89,28 +90,7 @@
         </div>
         <button  type="submit" class="btn btn-primary">Cadastrar</button>
     </div>
-    <script language="Javascript">
-      function validacaoEmail(field) {
-      usuario = field.value.substring(0, field.value.indexOf("@"));
-      dominio = field.value.substring(field.value.indexOf("@")+ 1, field.value.length);
-      if ((usuario.length >=1) &&
-          (dominio.length >=3) && 
-          (usuario.search("@")==-1) && 
-          (dominio.search("@")==-1) &&
-          (usuario.search(" ")==-1) && 
-          (dominio.search(" ")==-1) &&
-          (dominio.search(".")!=-1) &&      
-          (dominio.indexOf(".") >=1)&& 
-          (dominio.lastIndexOf(".") < dominio.length - 1)) {
-      document.getElementById("msgemail").innerHTML="E-mail válido";
-      alert("email valido");
-      }
-      else{
-      document.getElementById("msgemail").innerHTML="<font color='red'>Email inválido </font>";
-      alert("E-mail invalido");
-      }
-      }
-      </script>
+    
     
 
 </form>
--- efgds/resources/views/pizza.blade.php
+++ efgds/resources/views/pizza.blade.php
@@ -15,7 +15,7 @@
 <body>
 <style type="text/css">
   body{
-      background-image:  url(assets/img/pizza-background.jpg);;
+      background-image:  url(assets/img/pizza-background.jpg);
   }
    </style>
     <nav class="navbar navbar-light bg-light">
@@ -23,12 +23,49 @@
                 <a class="navbar-brand" href="pizza">Cardápio</a>
                 <a class="navbar-brand" href="{{url('contato')}}">Contato</a>
     </nav>
-    
---> <footer>
+    <form action="{{ url('pizza') }}">
+    @csrf
+        <h2>Cadastro</h2>
+        <div class="form-group col-md-2">
+                <label for="inputZip">Nome </label>
+                <input type="text" class="form-control" id="inputZip" nome="nome">
+            </div>
+            <div class="form-group col-md-4">
+                    <label for="inputState">Tamanho da Pizza</label>
+                    <select id="inputState" name="tamanho" class="form-control">
+                        <option selected>Escolha...</option>
+                        <option value="pequena">Pequena</option>
+                        <option value="media">Media</option>
+                        <option value="grande">Grande</option>
+                      </select>
+            </div> 
+             <div class="form-group col-md-4">
+                    <label for="inputState">Sabor escolhido</label>
+                    <select id="inputState" name="sabor" class="form-control">
+                        <option selected>Escolha...</option>
+                        <option value="1">Frango Com Catupiry</option>
+                        <option value="2">Calabresa</option>
+                        <option value="3">Portuguesa</option>
+                        <option value="4">Quatro queijos</option>
+                        <option value="5">Napolitana</option>
+                        <option value="6">Brigadeiro</option>
+                      </select>
+                </div>
+                <div class="form-group col-md-4">
+                        <label for="inputState">Forma de pagamento</label>
+                        <select id="inputState" name="pagamento" class="form-control">
+                            <option selected>Escolha...</option>
+                            <option value="dinheiro">Dinheiro</option>
+                            <option value="debito">Cartao Debito</option>
+                            <option value="credito">Cartao Credito</option>
+                          </select>
+                </div>
+             
+                <button  type="submit" class="btn btn-primary">Cadastrar Pedido</button>
+           </div>           
+    </section>
+            
+    </form>
       
-      <p>By: Erica Fernanda</p>
-    <p>Contato: <a href="mailto:ericanadagds@gmail.com">
-        ericanadagds@gmail.com</a></p>
-    </footer>       
 </body>
 </html>
